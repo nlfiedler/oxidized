451e777e2b83cc679e865fbac7e864a369cabe4a
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-18 18:19:12 -0500

    tests for OptionResultTransposer

---
+++ test/src/option_utils/option_result_transposer_test.dart
@@ -0,0 +1,25 @@
+import 'package:oxidized/oxidized.dart';
+import 'package:test/test.dart';
+
+typedef OptRes = Option<Result<int, String>>;
+typedef ResOpt = Result<Option<int>, String>;
+
+void main() {
+  test('None() transpose must return Ok(None())', () {
+    final OptRes input = None();
+    final ResOpt output = Ok(None());
+    expect(input.transpose(), equals(output));
+  });
+
+  test('Some(Ok(val)) transpose must return Ok(Some(val))', () {
+    final OptRes input = Some(Ok(1));
+    final ResOpt output = Ok(Some(1));
+    expect(input.transpose(), equals(output));
+  });
+
+  test('Some(Err(val)) transpose must return Err(val)', () {
+    final OptRes input = Some(Err('error'));
+    final ResOpt output = Err('error');
+    expect(input.transpose(), equals(output));
+  });
+}
