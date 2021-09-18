798a3930db02336b12e489a8f7abff04b5a3c0ef
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-18 18:27:03 -0500

    more docs

--- lib/src/option_utils/option_result_transposer.dart
+++ lib/src/option_utils/option_result_transposer.dart
@@ -4,6 +4,12 @@ extension OptionResultTransposer<T extends Object, E extends Object>
     on Option<Result<T, E>> {
   /// Transposes the result of an option.
   ///
+  /// ```dart
+  /// Some(Ok(1)) => Ok(Some(1));
+  /// Some(Err('error message')) => Err('error message');
+  /// None() => Ok(None());
+  /// ```
+  ///
   /// See also https://doc.rust-lang.org/std/option/enum.Option.html#method.transpose
   Result<Option<T>, E> transpose() {
     return match(
