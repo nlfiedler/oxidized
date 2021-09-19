74fc0fa8649be09efc8807733c19f4a31fef803f
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-18 19:00:54 -0500

    OptionOptionFlattener extension

---
+++ lib/src/option_utils/option_option_flattener.dart
@@ -0,0 +1,20 @@
+part of 'option_utils.dart';
+
+extension OptionOptionFlattener<T extends Object> on Option<Option<T>> {
+  /// Flats an option of an option.
+  ///
+  /// ```dart
+  /// Some(Some(1)) => Some(1)
+  /// Some(None()) => None()
+  /// None() => None()
+  /// ```
+  ///
+  /// See also:
+  /// - https://doc.rust-lang.org/std/option/enum.Option.html#method.flatten
+  Option<T> flatten() {
+    return match(
+      (option) => option,
+      () => None(),
+    );
+  }
+}
