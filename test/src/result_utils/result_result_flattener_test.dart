3b5fda9b83941cf018f9c0cf5d03f9619ea6a4f1
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-19 21:42:29 -0500

    tests for ResultResultFlattener

---
+++ test/src/result_utils/result_result_flattener_test.dart
@@ -0,0 +1,25 @@
+import 'package:oxidized/oxidized.dart';
+import 'package:test/test.dart';
+
+typedef Res = Result<int, String>;
+typedef ResRes = Result<Result<int, String>, String>;
+
+void main() {
+  test('Ok(Ok(val).flatten() must return Ok(val)', () async {
+    final ResRes input = Ok(Ok(1));
+    final Res output = Ok(1);
+    expect(input.flatten(), output);
+  });
+
+  test('Ok(Err(err).flatten() must return Err(err)', () async {
+    final ResRes input = Ok(Err('error message'));
+    final Res output = Err('error message');
+    expect(input.flatten(), output);
+  });
+
+  test('Err(err).flatten() must return Err(err)', () async {
+    final ResRes input = Err('error message');
+    final Res output = Err('error message');
+    expect(input.flatten(), output);
+  });
+}
