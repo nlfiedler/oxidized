e0c60f339d6da71de4371d64b78ff2fc796ea705
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-19 21:38:03 -0500

    adding ResultResultFlattener

---
+++ lib/src/result_utils/result_result_flattener.dart
@@ -0,0 +1,24 @@
+part of 'result_utils.dart';
+
+extension ResultResultFlattener<T extends Object, E extends Object>
+    on Result<Result<T, E>, E> {
+  /// Flattens a [Result<Result<T, E>, E>] into a [Result<T, E>]
+  ///
+  /// ```dart
+  /// Ok(Ok(value)) => Ok(value)
+  /// Ok(Err(error)) => Err(error)
+  /// Err(error) => Err(error)
+  /// ```
+  ///
+  /// See also:
+  /// - https://doc.rust-lang.org/std/result/enum.Result.html#method.flatten
+  Result<T, E> flatten() {
+    return match(
+      (result) => result.match(
+        (val) => Ok(val),
+        (err) => Err(err),
+      ),
+      (err) => Err(err),
+    );
+  }
+}
--- lib/src/result_utils/result_utils.dart
+++ lib/src/result_utils/result_utils.dart
@@ -1,3 +1,4 @@
 import 'package:oxidized/oxidized.dart';
 
 part 'result_option_transposer.dart';
+part 'result_result_flattener.dart';
