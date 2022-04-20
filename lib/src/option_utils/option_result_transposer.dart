06686f42aadde060a94d21f312874afc27847e11
Frank Moreno <frankmoreno1993@gmail.com>
2022-04-20 15:00:40 -0500

    fix docs for transposers

--- lib/src/option_utils/option_result_transposer.dart
+++ lib/src/option_utils/option_result_transposer.dart
@@ -1,12 +1,13 @@
 part of 'option_utils.dart';
 
+/// Utils for transposing [Option<Result<T, E>>] to [Result<Option<T>, E>]
 extension OptionResultTransposer<T extends Object, E extends Object>
     on Option<Result<T, E>> {
   /// Transposes the result of an option.
   ///
   /// ```dart
   /// Some(Ok(1)) => Ok(Some(1));
-  /// Some(Err('error message')) => Err('error message');
+  /// Some(Err('message')) => Err('message');
   /// None() => Ok(None());
   /// ```
   ///
@@ -17,19 +18,21 @@ extension OptionResultTransposer<T extends Object, E extends Object>
         (value) => Ok(Some(value)),
         (err) => Err(err),
       ),
-      () => Ok(None()),
+      () => Ok(None<T>()),
     );
   }
 }
 
+/// Utils for transposing [Future<Option<Result<T, E>>>]
+/// to [Future<Result<Option<T>, E>>]
 extension FutureOptionResultTransposer<T extends Object, E extends Object>
     on Future<Option<Result<T, E>>> {
   /// Transposes the result of an option.
   ///
   /// ```dart
-  /// Some(Ok(1)) => Ok(Some(1));
-  /// Some(Err('error message')) => Err('error message');
-  /// None() => Ok(None());
+  /// Future.value(Some(Ok(1))) => Future.value(Ok(Some(1)));
+  /// Future.value(Some(Err('message'))) => Future.value(Err('message'));
+  /// Future.value(None()) => Future.value(Ok(None()));
   /// ```
   ///
   /// See also https://doc.rust-lang.org/std/option/enum.Option.html#method.transpose
--- lib/src/result_utils/result_option_transposer.dart
+++ lib/src/result_utils/result_option_transposer.dart
@@ -1,5 +1,7 @@
 part of 'result_utils.dart';
 
+/// Utils for transposing [Result<Option<T>, E>] into [Option<Result<T, E>>]
+/// in a [Future]
 extension ResultOptionTransposer<T extends Object, E extends Object>
     on Result<Option<T>, E> {
   /// Transposes the result to an option.
@@ -16,21 +18,23 @@ extension ResultOptionTransposer<T extends Object, E extends Object>
     return match(
       (option) => option.match(
         (val) => Some(Ok(val)),
-        () => None(),
+        () => None<Result<T, E>>(),
       ),
       (err) => Some(Err(err)),
     );
   }
 }
 
+/// Utils for transposing [Option<Result<T, E>>] into [Result<Option<T>, E>]
+/// in a [Future]
 extension FutureResultOptionTransposer<T extends Object, E extends Object>
     on Future<Result<Option<T>, E>> {
   /// Transposes the result to an option.
   ///
   /// ```dart
-  /// Ok(None()) => None()
-  /// Ok(Some(val)) => Some(Ok(val))
-  /// Err(err) => Some(Err(err))
+  /// Future.value(Ok(None())) => Future.value(None())
+  /// Future.value(Ok(Some(val))) => Future.value(Some(Ok(val)))
+  /// Future.value(Err(err)) => Future.value(Some(Err(err)))
   /// ```
   ///
   /// See also:
