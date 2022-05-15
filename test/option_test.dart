8397055ffdd3d709109752056704d41b490e9bf0
Frank Moreno <frankmoreno1993@gmail.com>
2022-05-15 12:03:47 -0500

    refactor: OptionMapFilterMixin tests

--- test/option_test.dart
+++ test/option_test.dart
@@ -119,15 +119,6 @@ void main() {
       expect(called, equals(2));
     });
 
-    test('mapping values', () {
-      expect(Option.some(5).map((v) => v * 2).unwrap(), equals(10));
-      expect(Option.some(5).mapOr((v) => v * 2, 2), equals(10));
-      expect(Option.some(5).mapOrElse((v) => v * 2, () => 2), equals(10));
-      expect(Option.none().map((v) => fail('oh no')), isA<None>());
-      expect(Option<int>.none().mapOr((v) => fail('oh no'), 2), equals(2));
-      expect(None<int>().mapOrElse((v) => fail('oh no'), () => 2), equals(2));
-    });
-
     test('mapping values async', () async {
       expect(
         await Option.some(5)
@@ -174,12 +165,6 @@ void main() {
         await Option.none().okOrElseAsync(() async => Exception()),
         isA<Err>(),
       );
-    });
-
-    test('filtering options', () {
-      expect(Option.some(2).filter((v) => v.isEven), isA<Some>());
-      expect(Option.some(3).filter((v) => v.isEven), isA<None>());
-      expect(Option.none().filter((v) => fail('oh no')), isA<None>());
     });
 
     test('filtering options async', () async {
