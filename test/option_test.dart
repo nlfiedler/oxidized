3f0639efee7458bc4faa5ad093729bf366df1ca0
Frank Moreno <frankmoreno1993@gmail.com>
2022-05-15 12:22:41 -0500

    refactor: OptionLoginAsyncExtension tests

--- test/option_test.dart
+++ test/option_test.dart
@@ -125,33 +125,5 @@ void main() {
         isA<None>(),
       );
     });
-
-    test('this and that async', () async {
-      expect(
-        await Option.some(2)
-            .andThenAsync((v) async => Option.some(v * 2))
-            .then((v) => v.unwrap()),
-        equals(4),
-      );
-      expect(
-        await Option.none().andThenAsync((v) => fail('oh no')),
-        isA<None>(),
-      );
-    });
-
-    test('this or that async', () async {
-      expect(
-        await Option.some(2).orElseAsync(() => fail('oh no')),
-        isA<Some>(),
-      );
-      expect(
-        await Option.none().orElseAsync(() async => Option.none()),
-        isA<None>(),
-      );
-      expect(
-        await Option.none().orElseAsync(() async => Option.some(1)),
-        isA<Some>(),
-      );
-    });
   });
 }
