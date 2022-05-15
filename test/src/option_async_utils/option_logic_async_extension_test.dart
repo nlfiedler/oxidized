a892755fb437def264a86386619e0f92b21b477a
Frank Moreno <frankmoreno1993@gmail.com>
2022-05-15 13:08:21 -0500

    fix test for OptionLoginAsyncExtension

--- test/src/option_async_utils/option_logic_async_extension_test.dart
+++ test/src/option_async_utils/option_logic_async_extension_test.dart
@@ -4,33 +4,31 @@ import 'package:oxidized/oxidized.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('option logic async extension ...', () async {
-    test('this and that async', () async {
-      expect(
-        await Option.some(2)
-            .andThenAsync((v) async => Option.some(v * 2))
-            .then((v) => v.unwrap()),
-        equals(4),
-      );
-      expect(
-        await Option.none().andThenAsync((v) => fail('oh no')),
-        isA<None>(),
-      );
-    });
+  test('this and that async', () async {
+    expect(
+      await Option.some(2)
+          .andThenAsync((v) async => Option.some(v * 2))
+          .then((v) => v.unwrap()),
+      equals(4),
+    );
+    expect(
+      await Option.none().andThenAsync((v) => fail('oh no')),
+      isA<None>(),
+    );
+  });
 
-    test('this or that async', () async {
-      expect(
-        await Option.some(2).orElseAsync(() => fail('oh no')),
-        isA<Some>(),
-      );
-      expect(
-        await Option.none().orElseAsync(() async => Option.none()),
-        isA<None>(),
-      );
-      expect(
-        await Option.none().orElseAsync(() async => Option.some(1)),
-        isA<Some>(),
-      );
-    });
+  test('this or that async', () async {
+    expect(
+      await Option.some(2).orElseAsync(() => fail('oh no')),
+      isA<Some>(),
+    );
+    expect(
+      await Option.none().orElseAsync(() async => Option.none()),
+      isA<None>(),
+    );
+    expect(
+      await Option.none().orElseAsync(() async => Option.some(1)),
+      isA<Some>(),
+    );
   });
 }
