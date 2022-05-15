ca10cb85918617e4046a596f1a6e5c462a6baf6c
Frank Moreno <frankmoreno1993@gmail.com>
2022-05-15 13:41:02 -0500

    test for FutureOptionOptionFlattener

--- test/src/option_utils/option_option_flattener_test.dart
+++ test/src/option_utils/option_option_flattener_test.dart
@@ -7,21 +7,46 @@ typedef OptInt = Option<int>;
 typedef OptOptInt = Option<OptInt>;
 
 void main() {
-  test('None().flatten() must return None', () {
-    final OptOptInt input = None();
-    final OptInt output = None();
-    expect(input.flatten(), equals(output));
+  group('Option<Option<T>', () {
+    test('None().flatten() must return None', () {
+      final OptOptInt input = None();
+      final OptInt output = None();
+      expect(input.flatten(), equals(output));
+    });
+
+    test('Some(None()).flatten() must return None()', () {
+      final OptOptInt input = Some(None());
+      final OptInt output = None();
+      expect(input.flatten(), equals(output));
+    });
+
+    test('Some(Some(val)).flatten() must return Some(val)', () {
+      final OptOptInt input = Some(Some(1));
+      final OptInt output = Some(1);
+      expect(input.flatten(), equals(output));
+    });
   });
 
-  test('Some(None()).flatten() must return None()', () {
-    final OptOptInt input = Some(None());
-    final OptInt output = None();
-    expect(input.flatten(), equals(output));
-  });
+  group('Future<Option<Option<T>', () {
+    test('None().flatten() must return None', () async {
+      final OptOptInt input = None();
+      final futureInput = Future.value(input);
+      final OptInt output = None();
+      expect(await futureInput.flatten(), equals(output));
+    });
 
-  test('Some(Some(val)).flatten() must return Some(val)', () {
-    final OptOptInt input = Some(Some(1));
-    final OptInt output = Some(1);
-    expect(input.flatten(), equals(output));
+    test('Some(None()).flatten() must return None()', () async {
+      final OptOptInt input = Some(None());
+      final futureInput = Future.value(input);
+      final OptInt output = None();
+      expect(await futureInput.flatten(), equals(output));
+    });
+
+    test('Some(Some(val)).flatten() must return Some(val)', () async {
+      final OptOptInt input = Some(Some(1));
+      final futureInput = Future.value(input);
+      final OptInt output = Some(1);
+      expect(await futureInput.flatten(), equals(output));
+    });
   });
 }
